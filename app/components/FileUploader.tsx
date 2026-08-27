1b73f930daf6dca4164584b438ad9cfe3987d223
Ranica10 <ranicachawla101@gmail.com>
2026-08-26 21:34:06 -0400

    add remove button

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -46,6 +46,13 @@ const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
                                     <p className="text-sm text-gray-500">{formatSize(file.size)}</p>
                                 </div>
                             </div>
+
+                            {/* Remove uploaded pdf */}
+                            <button className="p-2 cursor-pointer" onClick={(e) => {
+                                onFileSelect?.(null)
+                            }}>
+                                <img src="/icons/cross.svg" alt="remove" className="w-4 h-4" />
+                            </button>
                         </div>
                     ) : (
                         // Nothing uploaded yet
