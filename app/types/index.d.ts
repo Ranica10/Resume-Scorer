04c589b347b128d8c10b510c8cb722902c013b20
Ranica10 <ranicachawla101@gmail.com>
2026-08-27 22:23:39 -0400

    add ai response format

--- app/types/index.d.ts
+++ app/types/index.d.ts
@@ -7,3 +7,47 @@ interface Resume {
   resumePath: string;
   feedback: Feedback;
 }
+
+// Define format for feedback from AI
+interface Feedback {
+  overallScore: number;
+  ATS: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+    }[];
+  };
+  toneAndStyle: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+      explanation: string;
+    }[];
+  };
+  content: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+      explanation: string;
+    }[];
+  };
+  structure: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+      explanation: string;
+    }[];
+  };
+  skills: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+      explanation: string;
+    }[];
+  };
+}
