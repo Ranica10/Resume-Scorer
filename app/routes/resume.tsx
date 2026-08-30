8d1157e1d06c1af435c9c1ee9f45aa18540a0bea
Ranica10 <ranicachawla101@gmail.com>
2026-08-30 18:38:58 -0400

    define all components for feedback

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router";
+import ATS from "~/components/ATS";
+import Details from "~/components/Details";
+import Summary from "~/components/Summary";
 import { usePuterStore } from "~/lib/puter";
 
-export const meta = () => {[
-    { title: "Resume Scorer | Review" },
-    { name: "description", content: "Detailed overview of your resume" },
-]}
+export const meta = () => {
+    [
+        { title: "Resume Scorer | Review" },
+        { name: "description", content: "Detailed overview of your resume" },
+    ]
+}
 
 const resume = () => {
     const { auth, isLoading, fs, kv } = usePuterStore();
@@ -15,9 +20,15 @@ const resume = () => {
 
     const [imageUrl, setImageUrl] = useState("");
     const [resumeUrl, setResumeUrl] = useState("");
-    const [feedback, setFeedback] = useState("");
+    const [feedback, setFeedback] = useState<Feedback | null>(null);
 
     const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isLoading && !auth.isAuthenticated) {
+            navigate(`/auth?next=/resume/${id}`);
+        }
+    }, [auth.isAuthenticated])
 
     useEffect(() => {
         const loadResume = async () => {
@@ -27,7 +38,7 @@ const resume = () => {
 
             // Get the data associated w/ the resume
             const data = JSON.parse(resume);
-            
+
             // Read the resume file
             const resumeBlob = await fs.read(data.resumePath)
             if (!resumeBlob) return;
@@ -44,9 +55,11 @@ const resume = () => {
 
             // Create a URL for the image file to display it in the browser
             const imageUrl = URL.createObjectURL(imageBlob)
-            setImageUrl(imageUrl); 
+            setImageUrl(imageUrl);
 
-            console.log({resumeUrl, imageUrl, feedback: data.feedback})
+            setFeedback(data.feedback);
+
+            // console.log({resumeUrl, imageUrl, feedback: data.feedback})
         }
 
         // Load the resume
@@ -65,16 +78,32 @@ const resume = () => {
 
             <div className="flex flex-row w-full max-lg:flex-col-reverse">
                 <section className="feedback-section bg-[url('/images/bg_small.svg')] bg-cover h-[100vh] sticky top-0 items-center justify-center">
+                    {/* Display the resume as an image in the browser */}
                     {imageUrl && resumeUrl && (
                         <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
+                            {/* Click on the img to open the actual pdf */}
                             <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
-                                <img 
+                                <img
                                     src={imageUrl}
                                     className="w-full h-full object-contain rounded-2xl"
                                     title="resume"
                                 />
                             </a>
                         </div>
+                    )}
+                </section>
+
+                <section className="feedback-section">
+                    <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
+
+                    {feedback ? (
+                        <div className="flex flec-col gap-8 animate-in fade-in duration-1000">
+                            <Summary feedback={feedback} />
+                            <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
+                            <Details feedback={feedback} />
+                        </div>
+                    ) : (
+                        <img src="/images/resume-scan-2.gif" className="w-full" />
                     )}
                 </section>
             </div>
