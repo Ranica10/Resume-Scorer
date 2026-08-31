725baf720315138475c8dfc48a0cd31d7865010b
Ranica10 <ranicachawla101@gmail.com>
2026-08-30 22:23:15 -0400

    add score badge

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,3 +1,4 @@
+import ScoreBadge from "./ScoreBadge";
 import ScoreGauge from "./ScoreGauge"
 
 // Different categories of resume feedback with their respective scores
@@ -13,6 +14,7 @@ const Category = ({ title, score }: { title: string, score: number }) => {
             <div className="category">
                 <div className="flex flex-row gap-2 items-center justify-center">
                     <p className="text-2xl">{title}</p>
+                    <ScoreBadge score={score} />
                 </div>
                 <p className="text-2xl">
                     {/* The score is displayed in a color that indicates its level */}
