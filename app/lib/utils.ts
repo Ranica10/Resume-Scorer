79e2ee4ea1448cba4864efe7466aa2c6468ed857
Ranica10 <ranicachawla101@gmail.com>
2026-08-30 22:30:39 -0400

    add ATS section

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,3 +1,10 @@
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 /**
  * Format a file size in bytes into a human-readable string (Bytes, KB, MB, GB, TB)
  * @param bytes 
