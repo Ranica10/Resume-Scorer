7deeeb0031290395f473c793ceb1f6f9fc084bcf
Ranica10 <ranicachawla101@gmail.com>
2026-08-30 21:26:45 -0400

    work on summary section

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,8 +1,48 @@
+import ScoreGauge from "./ScoreGauge"
 
-const Summary = () => {
-  return (
-    <div>Summary</div>
-  )
+// Different categories of resume feedback with their respective scores
+const Category = ({ title, score }: { title: string, score: number }) => {
+    // Determine the text color based on the score
+    const textColour = score > 70 ? 'text-green-600'
+                        : score > 49 ? 'text-yellow-600'
+                        : 'text-red-600';
+
+    return (
+        <div className="resume-summary">
+            {/* Each category displays its title and score */}
+            <div className="category">
+                <div className="flex flex-row gap-2 items-center justify-center">
+                    <p className="text-2xl">{title}</p>
+                </div>
+                <p className="text-2xl">
+                    {/* The score is displayed in a color that indicates its level */}
+                    <span className={textColour}>{score}</span>/100
+                </p>
+            </div>
+        </div>
+    )
+}
+
+const Summary = ({ feedback }: { feedback: Feedback }) => {
+    return (
+        <div className="bg-white rounded-2xl shadow-md w-full">
+            {/* Gauge graphic w/ score progress bar */}
+            <div className="flex flex-row items-center p-4 gap-8">
+                <ScoreGauge score={feedback.overallScore} />
+
+                <div className="flex flex-col gap-2">
+                    <h2 className="text-2xl font-bold">Your Resume Score</h2>
+                    <p className="text-sm text-gray-500">This score is calculated based on the variables below:</p>
+                </div>
+            </div>
+
+            {/* List of categories with their scores */}
+            <Category title="Tone & Style" score={feedback.toneAndStyle.score} />
+            <Category title="Content" score={feedback.content.score} />
+            <Category title="Structure" score={feedback.structure.score} />
+            <Category title="Skills" score={feedback.skills.score} />
+        </div>
+    )
 }
 
 export default Summary
--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -97,7 +97,7 @@ const resume = () => {
                     <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
 
                     {feedback ? (
-                        <div className="flex flec-col gap-8 animate-in fade-in duration-1000">
+                        <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
                             <Summary feedback={feedback} />
                             <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
                             <Details feedback={feedback} />
